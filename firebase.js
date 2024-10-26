7278c376c7aee0342ad6b8860927eb872882c15d
blockDeepanshu <chauhandeepanshu336@gmail.com>
2024-10-26 19:19:10 +0530

    feat: add firebase login

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTB9e3_tw9ITCBAKyetiE17ud0bFZ20b8",
-  authDomain: "fir-kart-9e88e.firebaseapp.com",
-  projectId: "fir-kart-9e88e",
-  storageBucket: "fir-kart-9e88e.appspot.com",
-  messagingSenderId: "225470722292",
-  appId: "1:225470722292:web:746429f8b6035bab05aea1",
-  measurementId: "G-HTTBSG6Q9G"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
